2a31da13641c89b37f114275d6850f75796c452e
Dushko Zashev <zashevd@myumanitoba.ca>
2018-06-08 12:22:11 -0500

    Changed bottom navigation bar

--- app/src/main/java/com/nitrogen/myme/MainActivity.java
+++ app/src/main/java/com/nitrogen/myme/MainActivity.java
@@ -26,10 +26,10 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_home:
                     //mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
+                case R.id.navigation_explore:
                     //mTextMessage.setText(R.string.title_dashboard);
                     return true;
-                case R.id.navigation_notifications:
+                case R.id.navigation_studio:
                     //mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
