f2e59342078ad966d33797df62cf710b5db4888a
Kevin <adamsk34@myumanitoba.ca>
2018-06-10 13:22:41 -0500

    not working crashes on createactivity

--- app/src/main/java/com/nitrogen/myme/CreateActivity.java
+++ app/src/main/java/com/nitrogen/myme/CreateActivity.java
@@ -1,14 +1,44 @@
 package com.nitrogen.myme;
 
 import android.content.Intent;
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
+import com.nitrogen.myme.textEditor.Font;
+import com.nitrogen.myme.textEditor.FontProvider;
+import com.nitrogen.myme.textEditor.MotionEntity;
+import com.nitrogen.myme.textEditor.MotionView;
+import com.nitrogen.myme.textEditor.TextEditorDialogFragment;
+import com.nitrogen.myme.textEditor.TextEntity;
+import com.nitrogen.myme.textEditor.TextLayer;
+
 public class CreateActivity extends AppCompatActivity {
+
+    protected MotionView motionView;
+    protected View textEntityEditPanel;
+    private final MotionView.MotionViewCallback motionViewCallback = new MotionView.MotionViewCallback() {
+        @Override
+        public void onEntitySelected(@Nullable MotionEntity entity) {
+            if (entity instanceof TextEntity) {
+                textEntityEditPanel.setVisibility(View.VISIBLE);
+            } else {
+                textEntityEditPanel.setVisibility(View.GONE);
+            }
+        }
+
+        @Override
+        public void onEntityDoubleTap(@NonNull MotionEntity entity) {
+            startTextEntityEditing();
+        }
+    };
+    private FontProvider fontProvider;
 
     // I duplicated this in MainActivity.java because I suck - Kevin
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -34,7 +64,7 @@ public class CreateActivity extends AppCompatActivity {
     };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
 
@@ -42,6 +72,69 @@ public class CreateActivity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         TextView title = findViewById(R.id.activityTitleCreate);
-        title.setText("This is the CREATE activity.") ;
+        title.setText("This is the CREATE activity.");
+
+        // adding text...
+        this.fontProvider = new FontProvider(getResources());
+        motionView = (MotionView) findViewById(R.id.main_motion_view);
+        textEntityEditPanel = findViewById(R.id.main_motion_text_entity_edit_panel);
+        motionView.setMotionViewCallback(motionViewCallback);
+        // should be called on press of a button
+        addTextSticker();
+    }
+
+    // text editing
+    protected void addTextSticker() {
+        TextLayer textLayer = createTextLayer();
+        TextEntity textEntity = new TextEntity(textLayer, motionView.getWidth(),
+                                                motionView.getHeight(), fontProvider);
+        motionView.addEntityAndPosition(textEntity);
+
+        // move text sticker up so that its not hidden under keyboard
+        PointF center = textEntity.absoluteCenter();
+        center.y = center.y * 0.5F;
+        textEntity.moveCenterTo(center);
+
+        // redraw
+        motionView.invalidate();
+
+        startTextEntityEditing();
+    }
+
+    // text editing
+    @Nullable
+    private TextEntity currentTextEntity() {
+        if (motionView != null && motionView.getSelectedEntity() instanceof TextEntity) {
+            return ((TextEntity) motionView.getSelectedEntity());
+        } else {
+            return null;
+        }
+    }
+
+    // text editing
+    private void startTextEntityEditing() {
+        TextEntity textEntity = currentTextEntity();
+        if (textEntity != null) {
+            TextEditorDialogFragment fragment = TextEditorDialogFragment.getInstance(textEntity.getLayer().getText());
+            fragment.show(getFragmentManager(), TextEditorDialogFragment.class.getName());
+        }
+    }
+
+    // text editing
+    private TextLayer createTextLayer() {
+        TextLayer textLayer = new TextLayer();
+        Font font = new Font();
+
+        font.setColor(TextLayer.Limits.INITIAL_FONT_COLOR);
+        font.setSize(TextLayer.Limits.INITIAL_FONT_SIZE);
+        font.setTypeface(fontProvider.getDefaultFontName());//  < runtime error
+
+        textLayer.setFont(font);
+
+        if (BuildConfig.DEBUG) {
+            textLayer.setText("Hello, world :)");
+        }
+
+        return textLayer;
     }
 }
