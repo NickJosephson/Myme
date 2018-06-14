25517691b58579cec4aecc24e9169c37c6ac2e92
Nicholas Josephson <josephs6@myumanitoba.ca>
2018-06-14 07:13:20 -0500

    Cleaned up FavouritesActivity.

--- app/src/main/java/com/nitrogen/myme/presentation/FavouritesActivity.java
+++ app/src/main/java/com/nitrogen/myme/presentation/FavouritesActivity.java
@@ -1,5 +1,7 @@
 package com.nitrogen.myme.presentation;
 
+import java.util.List;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -12,16 +14,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.SearchView;
 
 import com.nitrogen.myme.R;
 import com.nitrogen.myme.business.AccessFavourites;
-import com.nitrogen.myme.business.SearchMemes;
 import com.nitrogen.myme.objects.Meme;
-import com.nitrogen.myme.objects.Tag;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class FavouritesActivity extends AppCompatActivity {
     private AccessFavourites accessMemes;
@@ -29,13 +25,62 @@ public class FavouritesActivity extends AppCompatActivity {
     private MemesRecyclerAdapter adapter;
     private RecyclerView rvMemes;
     private boolean layoutAsGrid = true;
-    private SearchMemes searchMemes;
+
+    //**************************************************
+    // Activity Lifecycle
+    //**************************************************
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favourites);
+
+        // Setup toolbar
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        // Set up bottom navigation
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        Menu bottomNavMenu = navigation.getMenu();
+        MenuItem item = bottomNavMenu.getItem(1);
+        item.setChecked(true);
+
+        // Initialize memes
+        accessMemes = new AccessFavourites();
+        memes = accessMemes.getMemes();
+
+        // Setup recycler view
+        setupRV();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //data may have changed (e.g. some meme is now a favorite)
+        rvMemes.getAdapter().notifyDataSetChanged();
+    }
+
+    //**************************************************
+    // Activity Events
+    //**************************************************
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_bar, menu);
+
+        menu.removeItem(R.id.search);
+
+        setRVLayoutIcon(layoutAsGrid, menu.findItem(R.id.grid_toggle));
+        return super.onCreateOptionsMenu(menu);
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.grid_toggle:
-                // User chose the "Settings" item, show the app settings UI...
+                // User chose the grid toggle item
                 layoutAsGrid = !layoutAsGrid;
                 setRVLayout(layoutAsGrid);
                 setRVLayoutIcon(layoutAsGrid, item);
@@ -49,6 +94,52 @@ public class FavouritesActivity extends AppCompatActivity {
         }
     }
 
+    //**************************************************
+    // Navigation Events
+    //**************************************************
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_explore:
+                    startActivity(new Intent(FavouritesActivity.this, ExploreActivity.class));
+                    finish(); //end this activity
+                    return true;
+                case R.id.navigation_favourites:
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    //**************************************************
+    // Helper Methods
+    //**************************************************
+
+    /* setupRV
+     *
+     * purpose: Setup the recycler view to display the list of memes.
+     */
+    private void setupRV() {
+        // Lookup the recycler view in activity layout
+        rvMemes = (RecyclerView) findViewById(R.id.rvFavourites);
+
+        // Create adapter passing in the sample user data
+        adapter = new MemesRecyclerAdapter(memes);
+
+        // Attach the adapter to the recycler view to populate items
+        rvMemes.setAdapter(adapter);
+
+        // Set layout manager to position the items
+        setRVLayout(layoutAsGrid);
+    }
+
+    /* setRVLayout
+     *
+     * purpose: Set the recycler view to layout as gird or list.
+     */
     private void setRVLayout(boolean asGrid) {
         int numberOfColumns = 3;
 
@@ -59,6 +150,10 @@ public class FavouritesActivity extends AppCompatActivity {
         }
     }
 
+    /* setRVLayoutIcon
+     *
+     * purpose: Set the given grid view toggle icon.
+     */
     private void setRVLayoutIcon(boolean asGrid, MenuItem item) {
         if (layoutAsGrid) {
             item.setIcon(R.drawable.list_icon);
@@ -66,138 +161,5 @@ public class FavouritesActivity extends AppCompatActivity {
             item.setIcon(R.drawable.grid_icon);
         }
     }
-
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.activity_bar, menu);
-
-        MenuItem searchIcon = menu.findItem(R.id.search);
-        SearchView searchView = (SearchView)searchIcon.getActionView();
-
-        // handle input from the user
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String userInput) {
-                // filter through meme db
-                handleSearch(userInput);
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String userInput) {
-                // do nothing (this method definition is required by the constructor)
-                return false;
-            }
-        });
-
-        // if the user cancels their search, go back to initial meme list
-        searchView.setOnCloseListener(new SearchView.OnCloseListener() {
-            @Override
-            public boolean onClose() {
-                displayMemes(accessMemes.getMemes());
-                return false;
-            }
-        });
-
-        setRVLayoutIcon(layoutAsGrid, menu.findItem(R.id.grid_toggle));
-        return super.onCreateOptionsMenu(menu);
-    }
-
-
-    protected void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_favourites);
-
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-        Menu bottomNavMenu = navigation.getMenu();
-        MenuItem item = bottomNavMenu.getItem(1);
-        item.setChecked(true);
-
-
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
-
-
-
-        // Lookup the recycler view in activity layout
-        rvMemes = (RecyclerView) findViewById(R.id.rvFavourites);
-
-        // Initialize memes
-        accessMemes = new AccessFavourites();
-        memes = accessMemes.getMemes();
-
-        // Create adapter passing in the sample user data
-        adapter = new MemesRecyclerAdapter(memes);
-
-        // Attach the adapter to the recycler view to populate items
-        rvMemes.setAdapter(adapter);
-
-        // Set layout manager to position the items
-        setRVLayout(layoutAsGrid);
-
-        searchMemes = new SearchMemes();
-
-    }
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        rvMemes.getAdapter().notifyDataSetChanged();
-    }
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_explore:
-                    //mTextMessage.setText(R.string.title_home);
-                    startActivity(new Intent(FavouritesActivity.this, ExploreActivity.class));
-                    finish();
-                    return true;
-                case R.id.navigation_favourites:
-                    //mTextMessage.setText(R.string.title_dashboard);
-
-                    return true;
-//                case R.id.navigation_studio:
-//                    //mTextMessage.setText(R.string.title_notifications);
-//                    return true;
-            }
-            return false;
-        }
-    };
-
-
-    /* handleSearch
-     *
-     * purpose: take the user's input (the name of a tag) and perform a query to retrieve a
-     *          list of memes with that tag.
-     *
-     */
-    private void handleSearch(String input) {
-        String[] strings = input.split("\\s");
-        List<Tag> tags = new ArrayList<>();
-
-        for(int i = 0 ; i < strings.length; i ++) {
-            tags.add(new Tag(strings[i]));
-        }
-        memes =  searchMemes.getMemesByTags(tags);
-        displayMemes(memes);
-    }
-
-    /* displayMemes
-     *
-     * purpose: update the memes displayed on the screen.
-     */
-    private void displayMemes(List<Meme> memes) {
-        adapter.updateMemeList(memes);
-    }
-
-
 
 }
