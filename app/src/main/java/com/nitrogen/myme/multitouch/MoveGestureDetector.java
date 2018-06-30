9b8a4e9fff37faf4d013e18b33c697e47fa7f07f
Kevin <adamsk34@myumanitoba.ca>
2018-06-30 18:38:23 -0500

    removed unneeded code

--- app/src/main/java/com/nitrogen/myme/multitouch/MoveGestureDetector.java
+++ app/src/main/java/com/nitrogen/myme/multitouch/MoveGestureDetector.java
@@ -4,32 +4,10 @@ import android.content.Context;
 import android.graphics.PointF;
 import android.view.MotionEvent;
 
-/**
- * @author Almer Thie (code.almeros.com)
- *         Copyright (c) 2013, Almer Thie (code.almeros.com)
- *         <p>
- *         All rights reserved.
- *         <p>
- *         Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
- *         <p>
- *         Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
- *         Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer
- *         in the documentation and/or other materials provided with the distribution.
- *         <p>
- *         THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
- *         INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
- *         IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
- *         OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
- *         OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- *         OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
- *         OF SUCH DAMAGE.
- */
 public class MoveGestureDetector extends BaseGestureDetector {
 
     private static final PointF FOCUS_DELTA_ZERO = new PointF();
     private final OnMoveGestureListener mListener;
-    private PointF mCurrFocusInternal;
-    private PointF mPrevFocusInternal;
     private PointF mFocusExternal = new PointF();
     private PointF mFocusDeltaExternal = new PointF();
     public MoveGestureDetector(Context context, OnMoveGestureListener listener) {
@@ -86,30 +64,27 @@ public class MoveGestureDetector extends BaseGestureDetector {
 
         final MotionEvent prev = mPrevEvent;
 
-        // Focus intenal
-        mCurrFocusInternal = determineFocalPoint(curr);
-        mPrevFocusInternal = determineFocalPoint(prev);
+        // focus internal
+        PointF mCurrFocusInternal = determineFocalPoint(curr);
+        PointF mPrevFocusInternal = determineFocalPoint(prev);
 
-        // Focus external
-        // - Prevent skipping of focus delta when a finger is added or removed
+        // focus external
+        // prevent skipping of focus delta when a finger is added or removed
         boolean mSkipNextMoveEvent = prev.getPointerCount() != curr.getPointerCount();
         mFocusDeltaExternal = mSkipNextMoveEvent ? FOCUS_DELTA_ZERO : new PointF(mCurrFocusInternal.x - mPrevFocusInternal.x, mCurrFocusInternal.y - mPrevFocusInternal.y);
 
-        // - Don't directly use mFocusInternal (or skipping will occur). Add
-        // 	 unskipped delta values to mFocusExternal instead.
+        // don't directly use mFocusInternal (or skipping will occur)
+        // add unskipped delta values to mFocusExternal instead.
         mFocusExternal.x += mFocusDeltaExternal.x;
         mFocusExternal.y += mFocusDeltaExternal.y;
     }
 
-    /**
-     * Determine (multi)finger focal point (a.k.a. center point between all
-     * fingers)
+    /* determineFocalPoint
      *
-     * @param MotionEvent e
-     * @return PointF focal point
+     * purpose: Determine (multi)finger focal point (center point between all fingers)
      */
     private PointF determineFocalPoint(MotionEvent e) {
-        // Number of fingers on screen
+        // number of fingers on screen
         final int pCount = e.getPointerCount();
         float x = 0f;
         float y = 0f;
@@ -122,37 +97,28 @@ public class MoveGestureDetector extends BaseGestureDetector {
         return new PointF(x / pCount, y / pCount);
     }
 
-    public float getFocusX() {
-        return mFocusExternal.x;
-    }
-
-    public float getFocusY() {
-        return mFocusExternal.y;
-    }
-
     public PointF getFocusDelta() {
         return mFocusDeltaExternal;
     }
 
-    /**
-     * Listener which must be implemented which is used by MoveGestureDetector
-     * to perform callbacks to any implementing class which is registered to a
-     * MoveGestureDetector via the constructor.
+    /* OnMoveGestureListener
      *
-     * @see MoveGestureDetector.SimpleOnMoveGestureListener
+     * purpose: Listener which must be implemented which is used by MoveGestureDetector to perform
+     *     callbacks to any implementing class which is registered to a MoveGestureDetector via the
+     *     constructor.
      */
     public interface OnMoveGestureListener {
-        public boolean onMove(MoveGestureDetector detector);
+        boolean onMove(MoveGestureDetector detector);
 
-        public boolean onMoveBegin(MoveGestureDetector detector);
+        boolean onMoveBegin(MoveGestureDetector detector);
 
-        public void onMoveEnd(MoveGestureDetector detector);
+        void onMoveEnd(MoveGestureDetector detector);
     }
 
-    /**
-     * Helper class which may be extended and where the methods may be
-     * implemented. This way it is not necessary to implement all methods
-     * of OnMoveGestureListener.
+    /* SimpleOnMoveGestureListener
+     *
+     * purpose: Helper class which may be extended and where the methods may be implemented. This
+     *     way it is not necessary to implement all methods of OnMoveGestureListener.
      */
     public static class SimpleOnMoveGestureListener implements OnMoveGestureListener {
         public boolean onMove(MoveGestureDetector detector) {
@@ -164,7 +130,7 @@ public class MoveGestureDetector extends BaseGestureDetector {
         }
 
         public void onMoveEnd(MoveGestureDetector detector) {
-            // Do nothing, overridden implementation may be used
+            // do nothing, overridden implementation may be used
         }
     }
 
