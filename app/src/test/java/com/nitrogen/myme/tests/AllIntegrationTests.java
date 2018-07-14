563a818fa82dbd04574812fd011ac07aa192d3ae
Kevin <adamsk34@myumanitoba.ca>
2018-07-14 11:31:19 -0500

    1 working integration test based off sample

---
+++ app/src/test/java/com/nitrogen/myme/tests/AllIntegrationTests.java
@@ -0,0 +1,16 @@
+package com.nitrogen.myme.tests;
+
+import com.nitrogen.myme.tests.Business.AccessFavouritesIT;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        AccessFavouritesIT.class
+})
+public class AllIntegrationTests
+{
+    /* empty */
+}
