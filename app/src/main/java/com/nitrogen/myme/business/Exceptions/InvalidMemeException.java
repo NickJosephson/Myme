a1d59573e4fdd65fac60b55a525bd9668bf018a4
Kevin <adamsk34@myumanitoba.ca>
2018-07-13 21:55:39 -0500

    exceptions no longer stinky

--- app/src/main/java/com/nitrogen/myme/business/Exceptions/InvalidMemeException.java
+++ app/src/main/java/com/nitrogen/myme/business/Exceptions/InvalidMemeException.java
@@ -1,7 +1,4 @@
 package com.nitrogen.myme.business.Exceptions;
 
 public class InvalidMemeException extends RuntimeException {
-/*    public InvalidMemeException() {
-        super();
-    }
-*/}
+}
--- app/src/main/java/com/nitrogen/myme/business/Exceptions/MemeNotFoundException.java
+++ app/src/main/java/com/nitrogen/myme/business/Exceptions/MemeNotFoundException.java
@@ -1,7 +1,4 @@
 package com.nitrogen.myme.business.Exceptions;
 
 public class MemeNotFoundException extends RuntimeException {
-    public MemeNotFoundException(final String message) {
-        super(message);
-    }
 }
