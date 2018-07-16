3367d99e8f0a9974ba8a5dcb94ccc3233ca40c4d
Nicholas Josephson <josephs6@myumanitoba.ca>
2018-07-16 01:03:48 -0500

    Now using Mockito for SortMemes Unit test

--- app/src/test/java/com/nitrogen/myme/tests/Business/SortMemesTest.java
+++ app/src/test/java/com/nitrogen/myme/tests/Business/SortMemesTest.java
@@ -11,6 +11,8 @@ import com.nitrogen.myme.persistence.TagsPersistence;
 import com.nitrogen.myme.persistence.stubs.MemesPersistenceStub;
 import com.nitrogen.myme.persistence.stubs.TagsPersistenceStub;
 
+import static org.mockito.Mockito.*;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -25,29 +27,38 @@ import static org.junit.Assert.assertTrue;
 
 
 public class SortMemesTest {
-    private AccessMemes accessMemes;
-    private AccessFavourites accessFavourites;
-    private AccessTags accessTags;
     private List<Meme> memes;
     private MemesPersistence memesPersistenceStub;
+    private Tag tag1 = new Tag("tag1");
+    private Tag tag2 = new Tag("tag2");
 
     @Before
     public void setUp() {
         System.out.println("Starting tests for SearchTags.\n");
         // stub database
-        TagsPersistence tagsPersistenceStub = new TagsPersistenceStub();
-        memesPersistenceStub = new MemesPersistenceStub(tagsPersistenceStub);
+        TagsPersistence tagsPersistenceStub = mock(TagsPersistenceStub.class);
+        List<Tag> tags = new LinkedList<>();
+        tags.add(tag1);
+        tags.add(tag2);
+        when(tagsPersistenceStub.getTags()).thenReturn(tags);
 
-        accessMemes = new AccessMemes(memesPersistenceStub);
-        accessFavourites = new AccessFavourites(memesPersistenceStub);
-        accessTags = new AccessTags(tagsPersistenceStub);
-        //memes = new LinkedList<>();
-        assertNotNull(accessMemes);
-        assertNotNull(accessTags);
-        //assertNotNull(memes);
+        List<Meme> memes = new ArrayList<>(); // implementation specific
+        Meme fav1 = new Meme("fav1");
+        fav1.setFavourite(true);
+        memes.add(fav1);
+        Meme fav2 = new Meme("fav2");
+        fav2.addTag(tag1);
+        fav2.setFavourite(true);
+        memes.add(fav2);
+        Meme fav3 = new Meme("fav3");
+        fav3.addTag(tag1);
+        fav3.addTag(tag2);
+        fav3.setFavourite(true);
+        memes.add(fav3);
 
-        // Initialize memes
-        //memes.addAll(accessMemes.getMemes());
+        memesPersistenceStub = mock(MemesPersistenceStub.class);
+        when(memesPersistenceStub.getMemes()).thenReturn(memes);
+
     }
 
     @Test
@@ -62,12 +73,6 @@ public class SortMemesTest {
     public void testSortMemesByRelevance_multipleMemes() {
         System.out.println("Testing sortByRelevance() on multiple memes.");
 
-        // generate random index for tags
-        final int T_INDEX = (int)(Math.random() * (accessTags.getTags().size() - 2));
-
-        Tag tag1 = accessTags.getTags().get(T_INDEX);
-        Tag tag2 = accessTags.getTags().get(T_INDEX + 1);
-
         List<Meme> memes = new ArrayList<>(); // implementation specific
         Meme meme1 = new Meme("meme1");
         memes.add(meme1);
@@ -78,19 +83,6 @@ public class SortMemesTest {
         meme3.addTag(tag1);
         meme3.addTag(tag2);
         memes.add(meme3);
-
-        Meme fav1 = new Meme("fav1");
-        fav1.setFavourite(true);
-        memesPersistenceStub.insertMeme(fav1);
-        Meme fav2 = new Meme("fav2");
-        fav2.addTag(tag1);
-        fav2.setFavourite(true);
-        memesPersistenceStub.insertMeme(fav2);
-        Meme fav3 = new Meme("fav3");
-        fav3.addTag(tag1);
-        fav3.addTag(tag2);
-        fav3.setFavourite(true);
-        memesPersistenceStub.insertMeme(fav3);
 
         //check memes are in order inserted
         assertTrue(memes.get(0).equals(meme1));
@@ -109,12 +101,6 @@ public class SortMemesTest {
     @Test
     public void testSortMemesByRelevance_sortedMemesAtBottom() {
         System.out.println("Testing sortByRelevance() sorts favorite memes to the bottom.");
-
-        // generate random index for tags
-        final int T_INDEX = (int)(Math.random() * (accessTags.getTags().size() - 2));
-
-        Tag tag1 = accessTags.getTags().get(T_INDEX);
-        Tag tag2 = accessTags.getTags().get(T_INDEX + 1);
 
         List<Meme> memes = new ArrayList<>(); // implementation specific
         Meme meme1 = new Meme("meme1");
